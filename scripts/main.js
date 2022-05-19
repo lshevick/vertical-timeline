dd084a2daa8790a198529958543724ea80ee1d73
Levi Shevick <levidshevick@gmail.com>
2022-05-18 23:09:05 -0400

    adds photos and function for more/less button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 const cards = document.querySelectorAll('.timeline-card');
+const button = document.querySelectorAll('.more-button');
+const more = document.querySelectorAll('.more-info')
 
 function elementInView(e) {
     let rect = e.getBoundingClientRect();
     return (
         rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.left >= 0 &&
-        rect.right >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        rect.right >= 0 
     );
 }
 
@@ -20,5 +21,19 @@ function showCard() {
     }
 }
 
+for (let i = 0; i < button.length; i++) {
+    button[i].onclick = function() {
+        // for (let i = 0; i < more.length; i++) {
+            if (more[i].className === 'more-info open') {
+                more[i].classList.remove('open');
+                button[i].innerHTML = 'More';
+            } else {
+                more[i].classList.add('open');
+                button[i].innerHTML = 'Less'
+            }
+        // }
+        
+    }   
+}
 window.addEventListener('load', showCard);
 window.addEventListener('scroll', showCard);
